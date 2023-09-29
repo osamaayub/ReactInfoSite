d0c41d78bd5fc655718190727c7d2593572993bf
Osama ayub <ayubosama416@gmail.com>
2023-09-29 22:40:42 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Card from './components/Card'
+
+export default function App() {
+    return (
+        <div className="container">
+            <Card />
+        </div>
+    )
+}
